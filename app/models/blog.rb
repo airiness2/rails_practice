e306319b177568d502a1f68c3c107754feb0ce11
Reiko Watanabe <blackgothy@gmail.com>
2019-05-18 06:16:15 +0000

    updated blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,27 @@
 class Blog < ApplicationRecord
+  #  validates :title, presence: true, length: {maximum: 140}
+  #  validates :content, presence: true, length: {maximum: 140}
+
+
+  validate :add_error
+ 
+  def add_error
+    if title.blank?
+      errors[:base] << "タイトルを入力してください"
+    end
+    
+    if title.length >= 140
+        errors[:base] << "タイトルは140字以内で入力してください"
+    end
+ 
+    if content.blank?
+      errors[:base] << "本文を入力してください"
+    end
+
+    if content.length >= 140
+        errors[:base] << "本文は140字以内で入力してください"
+    end
+
+  end
+  
 end
